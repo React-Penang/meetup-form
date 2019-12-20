869a0082e86249b4cbeb4a292340e2e75a14799b
Jian C <jianc@whiteroomanalytics.com>
2019-12-21 02:56:19 +0800

    Update to meetup#2
    - Minor text updates
    - Update firestore rules

--- src/vars/firebase.js
+++ src/vars/firebase.js
@@ -1,10 +1,10 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase';
 import {initializeApp} from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyAOMgybQymcqnEADADXbl7YtzylZLOJQ_Y",
-  authDomain: "meetup-react-penang.firebaseapp.com",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "meetup-react-penang.web.app",
   databaseURL: "https://react-penang.firebaseio.com",
   projectId: "react-penang",
 });
